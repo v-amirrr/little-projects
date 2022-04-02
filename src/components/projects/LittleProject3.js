8e995e14f3f3de209b837f28e08c8e402d6da592
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-04-02 13:03:19 +0430

    created project 3

--- src/components/projects/LittleProject3.js
+++ src/components/projects/LittleProject3.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import {AwesomeButton} from "react-awesome-button";
+import "react-awesome-button/dist/themes/theme-blue.css";
+
 import styles from "./LittleProject3.module.css";
 
 const LittleProject3 = () => {
     return (
         <>
             <div className={styles.littleProject3}>
-                
+                <AwesomeButton size="large" type="primary">Button</AwesomeButton>
             </div>
         </>
     );
