d334f83a08f4a8cb5f5cb5802857221687cdda46
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-04-10 20:26:24 +0430

    added a back button for projects' pages
    We use this button to come back to the our home page from any page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+    const location = useLocation();
     return (
         <>
             <div className={styles.navbar}>
                 <div className={styles.navbarName}>
                    <h1>Little <span>PROJECTS</span></h1>
                 </div>
+                <Link to="/">
+                    <div className={location.pathname !== "/" ? styles.backBtn : styles.backBtnOff}>
+                        <i><BsFillArrowLeftCircleFill /></i>
+                        <span>Back To Home</span>
+                    </div>
+                </Link>
             </div>
         </>
     );
